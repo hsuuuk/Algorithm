2f60fc2fa20d707bf74901d0b0e42245a4a69d59
SIM HYUN SUK <114726674+hsuuuk@users.noreply.github.com>
2023-12-12 09:35:56 +0900

    [level 1] Title: 실패율, Time: 0.16 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42889. 실패율/실패율.swift
@@ -0,0 +1,48 @@
+import Foundation
+
+func solution(_ N:Int, _ stages:[Int]) -> [Int] {
+//    var failurePercent = [Int: Double]()
+//
+//    for i in 1...N {
+//        let failure = stages.filter { $0 == i }.count
+//        let player = stages.filter { $0 >= i }.count
+//        failurePercent[i] = Double(failure) / Double(player)
+//    }
+//
+//    let result = failurePercent.sorted(by: <).sorted(by: { $0.value > $1.value })
+//
+//    return result.map { $0.key }
+    
+//    var dic = [Int: Double]()
+//    var player = stages.count
+//
+//    for i in 1...N {
+//        let current = stages.filter { $0 == i }.count
+//        player -= current
+//
+//        let ratio = Double(current) / Double(player)
+//        dic[i] = ratio
+//    }
+//
+//    let result  = dic.sorted(by: <).sorted(by: { $0.value > $1.value }).map { $0.key }
+//    return result
+    
+    var dic = [Int: Double]()
+    var player = stages.count
+
+    for i in 1...N {
+        var current = 0
+        for j in 0..<stages.count {
+            if stages[j] == i {
+                current += 1
+            }
+        }
+        player -= current
+
+        let ratio = Double(current) / Double(player)
+        dic[i] = ratio
+    }
+
+    let result  = dic.sorted(by: <).sorted(by: { $0.value > $1.value }).map { $0.key }
+    return result
+}
