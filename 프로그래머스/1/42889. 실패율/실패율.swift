7094fdb1517819163fd3df14c8d73e84a221bf85
SIM HYUN SUK <114726674+hsuuuk@users.noreply.github.com>
2023-12-12 19:05:50 +0900

    [level 1] Title: 실패율, Time: 0.18 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.swift
+++ 프로그래머스/1/42889. 실패율/실패율.swift
@@ -27,6 +27,8 @@ func solution(_ N:Int, _ stages:[Int]) -> [Int] {
 //    let result  = dic.sorted(by: <).sorted(by: { $0.value > $1.value }).map { $0.key }
 //    return result
     
+    // 개선사항1️⃣: for문 안에서 고차함수를 사용하면 시간복잡도 상승 > 고차함수를 for문으로 교체
+    
     var dic = [Int: Double]()
     var player = stages.count
 
