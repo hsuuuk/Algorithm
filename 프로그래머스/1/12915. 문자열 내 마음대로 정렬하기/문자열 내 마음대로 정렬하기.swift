38fa7ec29fe56a66909caae8811799c381bb411a
SIM HYUN SUK <114726674+hsuuuk@users.noreply.github.com>
2023-12-14 17:17:05 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 25.33 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+func solution(_ strings:[String], _ n:Int) -> [String] {
+    return strings.sorted {
+        if Array($0)[n] == Array($1)[n] {
+            return $0 < $1
+        }
+        return Array($0)[n] < Array($1)[n]
+    }
+}
