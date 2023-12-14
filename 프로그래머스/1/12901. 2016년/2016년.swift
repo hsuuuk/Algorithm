83a3189d7fa2ecde247092ea16d202e5acdd72ac
SIM HYUN SUK <114726674+hsuuuk@users.noreply.github.com>
2023-12-14 18:25:27 +0900

    Update 2016년.swift

--- 프로그래머스/1/12901. 2016년/2016년.swift
+++ 프로그래머스/1/12901. 2016년/2016년.swift
@@ -1,5 +1,6 @@
-func solution(_ a: Int, _ b: Int) -> String {
-    let weekday = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]
-    let daysInMonths = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    return weekday[(daysInMonths[0..<a - 1].reduce(0, +) + b + 4) % 7]
+func solution(_ a:Int, _ b:Int) -> String {
+    let weekday = ["FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"]
+    let month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] // 윤년은 4의 배수인 년도(29일)
+    let index = (-1 + month[0..<a-1].reduce(0, +) + b) % 7
+    return weekday[index]
 }
