323be194bd35126a6eca5528ddf160cc980c8523
SIM HYUN SUK <114726674+hsuuuk@users.noreply.github.com>
2023-12-20 19:18:21 +0900

    [level 2] Title: 타겟 넘버, Time: 0.41 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.swift
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.swift
@@ -1,21 +1,20 @@
 import Foundation
 
 func solution(_ numbers:[Int], _ target:Int) -> Int {
-    var count = 0
+    var cnt = 0
     
     func dfs(index: Int, sum: Int) {
         if index == numbers.count-1 && sum == target {
-            count += 1
+            cnt += 1
             return
         } else if index == numbers.count-1 {
-            return 
+            return
         }
         
-        dfs(index: index+1, sum: sum + numbers[index+1])
-        dfs(index: index+1, sum: sum - numbers[index+1])
+        dfs(index: index + 1, sum: sum + numbers[index + 1])
+        dfs(index: index + 1, sum: sum - numbers[index + 1])
     }
-    
     dfs(index: -1, sum: 0)
     
-    return count
+    return cnt
 }
