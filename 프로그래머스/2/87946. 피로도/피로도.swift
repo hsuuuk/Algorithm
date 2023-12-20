57d906845513b98f0cc5e59d8bf339789ae9b6ec
SIM HYUN SUK <114726674+hsuuuk@users.noreply.github.com>
2023-12-20 17:39:38 +0900

    [level 2] Title: 피로도, Time: 0.36 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.swift
+++ 프로그래머스/2/87946. 피로도/피로도.swift
@@ -2,21 +2,21 @@ import Foundation
 
 func solution(_ k:Int, _ dungeons:[[Int]]) -> Int {
     var answer: Int = 0
-    dfs(k: k, dungeons: dungeons, count: 0, answer: &answer)
-    return answer
-}
-
-func dfs(k: Int, dungeons: [[Int]], count: Int, answer: inout Int) {
-    answer = max(count, answer)
-
-    for i in 0..<dungeons.count {
-        var dungeons = dungeons
-        
-        if dungeons[i][0] <= k {
-            let k = k - dungeons[i][1]
-            dungeons.remove(at: i)
-            let count = count + 1
-            dfs(k: k, dungeons: dungeons, count: count, answer: &answer)
+    
+    func dfs(_ k: Int, _ dungeons: [[Int]], _ count: Int, _ answer: inout Int) {
+        for dungeon in dungeons {
+            if k >= dungeon[0] {
+                let newK = k - dungeon[1]
+                var newDungeons = dungeons
+                newDungeons.removeAll(where: { $0 == dungeon })
+                let newCnt = count + 1
+                answer = max(answer, newCnt)
+                
+                dfs(newK, newDungeons, newCnt, &answer)
+            }
         }
     }
+    dfs(k, dungeons, 0, &answer)
+    
+    return answer
 }
