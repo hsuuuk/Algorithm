5494cb09443fab8a1f3acd3310d1bda4167dbba3
SIM HYUN SUK <114726674+hsuuuk@users.noreply.github.com>
2023-12-21 16:05:45 +0900

    Update 둘만의 암호.swift

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.swift
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.swift
@@ -1,21 +1,14 @@
 func solution(_ s:String, _ skip:String, _ index:Int) -> String {
-    let alphabet = "abcdefghijklmnopqrstuvwxyz".map { String($0) }
-    var s = s.map { String($0) }
+    let skip = Array(skip)
+    var eng = Array("abcdefghijklmnopqrstuvwxyz").filter { !skip.contains($0) }
     
-    for i in 0..<s.count {
-        if var c = alphabet.firstIndex(of: s[i]) {
-            var index = index
-            
-            while index != 0 {
-                c += 1
-                c %= 26
-                
-                if skip.contains(alphabet[c]) { continue }
-                
-                index -= 1
-                s[i] = alphabet[c]
-            }
+    var arr = Array(s)
+    for i in 0..<arr.count {
+        if let beforeIndex = eng.firstIndex(of: arr[i]) {
+            let afterIndex = (beforeIndex + index) % eng.count // index가 eng보다 두바퀴 더 클 가능성도 반드시 고려
+            arr[i] = eng[afterIndex]
         }
     }
-    return s.joined()
+    
+    return arr.map { String($0) }.joined()
 }
