943163dc7f249dabed968dd54fd18002dfdaa19c
SIM HYUN SUK <114726674+hsuuuk@users.noreply.github.com>
2023-12-21 12:27:30 +0900

    [level 2] Title: 피보나치 수, Time: 2.83 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.swift
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.swift
@@ -1,14 +1,9 @@
 func solution(_ n:Int) -> Int {
-    
-    if n < 2 {
-        return n
-    }
-    
     var fibo = [0, 1]
     
     for i in 2...n {
-        fibo.append((fibo[i-2] + fibo[i-1]) % 1234567)
+        fibo.append((fibo[i-1] + fibo[i-2]) % 1234567)
     }
-    
+
     return fibo[n]
 }
